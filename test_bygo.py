4c531c41b9624c774986c4cd2154ff34d2b4149c
os19216811ama-cyber <os192.168.1.1ama@gmail.com>
2026-06-03 15:20:36 +0500

    removed all page except one

--- test_bygo.py
+++ test_bygo.py
@@ -13,17 +13,6 @@ class LoadTest(unittest.TestCase):
         page_load_times = []
         threads = []
         page_list = [
-            ["Home Page","https://bygo.pk/",["https://bygo.pk/wp-content/uploads/2026/01/Kodak-Paper-1.jpg","https://bygo.pk/wp-content/uploads/2026/01/Logitec-Projector-1.jpg","https://bygo.pk/wp-content/uploads/2026/01/Logitec-cameras.jpg"]],
-            ["Shop Page", "https://bygo.pk/shop/",["https://bygo.pk/wp-content/uploads/2026/01/front-15-430x267.jpg","https://bygo.pk/wp-content/uploads/2026/01/front-image-2-1-430x267.jpg","https://bygo.pk/wp-content/uploads/2026/01/front-img-430x267.jpg"]],
-            ["Nvidia Jetson Orin Nano Page", "https://bygo.pk/nvidia-jetson-orin-nano/",["https://bygo.pk/wp-content/uploads/2025/12/WhatsApp-Image-2025-12-01-at-1.44.37-PM-2.jpeg","https://bygo.pk/wp-content/uploads/2025/12/WhatsApp-Image-2025-12-01-at-1.44.38-PM-800x700.jpeg"]],
-            ["Nvidia Jetson Orin Nano Shop Page", "https://bygo.pk/shop/nvidia-jetson-orin-nano/",["https://bygo.pk/wp-content/uploads/2025/12/2-2.png","https://bygo.pk/wp-content/uploads/2025/12/2-2-150x171.png","https://bygo.pk/wp-content/uploads/2025/12/3-150x171.png","https://bygo.pk/wp-content/uploads/2025/12/4-150x171.png"]],
-            ["S2 Screen Extender Shop Page", "https://bygo.pk/shop/s2-screen-extender/",["https://bygo.pk/wp-content/uploads/2025/10/S2-1.png","https://bygo.pk/wp-content/uploads/2025/10/S2-1-150x171.png","https://bygo.pk/wp-content/uploads/2025/10/S2-7-150x171.png","https://bygo.pk/wp-content/uploads/2025/10/S2-6-150x171.png"]],
-            ["Product Category Page For Logitech", "https://bygo.pk/product-category/logitech/",["https://bygo.pk/wp-content/uploads/2026/01/Logitech-GROUP-–-Conference-Room-Solution-3-430x491.jpg"]],
-            ["Kodak Page","https://bygo.pk/kodak/",["https://bygo.pk/wp-content/uploads/2025/12/BOOK-1.jpg","https://bygo.pk/wp-content/uploads/2026/01/gggg-1.png"]],
-            ["Metz Page","https://bygo.pk/metz-led/",["https://bygo.pk/wp-content/uploads/2025/12/1920-x-1080-Metzifp-2025-scaled.png"]],
-            ["About Us Page","https://bygo.pk/about-us-3/",["https://bygo.pk/wp-content/uploads/2025/12/Gemini_Generated_Image_o0mfroo0mfroo0mf.jpg"]],
-            ["NVIDIA Jetson Thor Developer Kit Shop Page","https://bygo.pk/shop/nvidia-jetson-thor-developer-kit/",["https://bygo.pk/wp-content/uploads/2026/01/71r8FRLSUjL._AC_SL1500_-150x115.jpg","https://bygo.pk/wp-content/uploads/2026/01/710TV4B1hAL._AC_SL1500_-150x84.jpg","https://bygo.pk/wp-content/uploads/2026/01/71Hhmo2XShL._AC_SL1500_-150x84.jpg"]],
-            ["GAC2500 — SIP/Android Conference Phone Shop Page","https://bygo.pk/shop/gac2500-sip-android-conference-phone/",["https://bygo.pk/wp-content/uploads/2026/01/GAC2500-SIP-Android-Video-Conferencing-Solution-2.jpg","https://bygo.pk/wp-content/uploads/2026/01/GAC2500-SIP-Android-Video-Conferencing-Solution-2-150x171.jpg","https://bygo.pk/wp-content/uploads/2026/01/GAC2500-SIP-Android-Video-Conferencing-Solution-1-150x171.jpg","https://bygo.pk/wp-content/uploads/2026/01/GAC2500-SIP-Android-Video-Conferencing-Solution-3-150x171.jpg"]],
             ["OBSBOT Tiny 2 AI-Powered PTZ 4K Webcam Shop Page","https://bygo.pk/shop/obsbot-tiny-2-ai-powered-ptz-4k-webcam/",["https://bygo.pk/wp-content/uploads/2026/01/tiny-2-main-image-2-150x88.jpg","https://bygo.pk/wp-content/uploads/2026/01/OBSBOT-Tiny-2-AI-Powered-PTZ-4K-Webcam-1-150x171.jpg","https://bygo.pk/wp-content/uploads/2026/01/OBSBOT-Tiny-2-AI-Powered-PTZ-4K-Webcam-2-150x171.jpg"]]
         ]
         def testbody():
